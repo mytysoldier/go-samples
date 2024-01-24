0b2be33a01b72afff50552681638acfa2ca31155
mytysoldier <yoshi008t@gmail.com>
2024-01-25 08:07:18 +0900

    add search and update function

--- gin-restapi/backend/server/funcs.go
+++ gin-restapi/backend/server/funcs.go
@@ -3,8 +3,22 @@ package server
 import (
 	"backend/model"
 	"database/sql"
+	"fmt"
 	"log"
 )
+
+func GetBookByID(db *sql.DB, id int) (model.Book, error) {
+	row := db.QueryRow("SELECT id, name, price FROM book WHERE id = $1", id)
+
+	var book model.Book
+	err := row.Scan(&book.Id, &book.Name, &book.Price)
+	if err != nil {
+		log.Fatal("error scanning book")
+		return model.Book{}, err
+	}
+
+	return book, nil
+}
 
 func GetBooks(db *sql.DB) ([]model.Book, error) {
 	rows, err := db.Query("SELECT id, name, price FROM book")
@@ -28,4 +42,36 @@ func GetBooks(db *sql.DB) ([]model.Book, error) {
 	}
 
 	return books, nil
+}
+
+func InsertBook(db *sql.DB, book model.Book) (model.Book, error) {
+	var insertedBook model.Book
+
+	err := db.QueryRow("INSERT INTO book (name, price) VALUES ($1, $2) RETURNING id, name, price", book.Name, book.Price).Scan(&insertedBook.Id, &insertedBook.Name, &insertedBook.Price)
+
+	if err != nil {
+		log.Fatal("error inserting book")
+		fmt.Println(err)
+		return model.Book{}, err
+	}
+
+	fmt.Println("inserted book")
+
+	return insertedBook, nil
+}
+
+func UpdateBook(db *sql.DB, book model.Book) (model.Book, error) {
+	var updatedBook model.Book
+
+	err := db.QueryRow("UPDATE book SET name=$1, price=$2 WHERE id=$3 RETURNING id, name, price", book.Name, book.Price, book.Id).Scan(&updatedBook.Id, &updatedBook.Name, &updatedBook.Price)
+
+	if err != nil {
+		log.Fatal("error updating book")
+		fmt.Println(err)
+		return model.Book{}, err
+	}
+
+	fmt.Println("updated book")
+
+	return updatedBook, nil
 }
--- gin-restapi/backend/server/server.go
+++ gin-restapi/backend/server/server.go
@@ -3,8 +3,10 @@ package server
 import (
 	"backend/db"
 	"backend/model"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +15,10 @@ func Server() {
 	r := gin.Default()
 	r.Use(corsMiddleware())
 	r.GET("/", handleHome)
+	r.GET("/book", handleSearchBook)
 	r.GET("/books", handleSearchBooks)
+	r.POST("/book", handleInsertBook)
+	r.PUT("/book/:id", handleUpdateBook)
 	r.Run()
 }
 
@@ -39,6 +44,27 @@ func handleHome(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"books": books})
 }
 
+func handleSearchBook(ctx *gin.Context) {
+	// リクエストパラメーター "id" を取得
+	id := ctx.Query("id")
+
+	bookID, err := strconv.Atoi(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	// GetBookByID 関数を呼び出し
+	book, err := GetBookByID(db.DB, bookID)
+	if err != nil {
+		log.Fatal(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve book"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"book": book})
+}
+
 func handleSearchBooks(ctx *gin.Context) {
 	books, err := GetBooks(db.DB)
 	if err != nil {
@@ -47,3 +73,59 @@ func handleSearchBooks(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, gin.H{"books": books})
 }
+
+func handleInsertBook(ctx *gin.Context) {
+	var book model.Book
+
+	// リクエストボディからデータを取得
+	if err := ctx.ShouldBindJSON(&book); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println("Bookリクエストデータ:", book)
+
+	book, err := InsertBook(db.DB, book)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"book": book})
+}
+
+func handleUpdateBook(ctx *gin.Context) {
+	// パスパラメーター "id" を取得
+	id := ctx.Param("id")
+
+	// id を整数に変換
+	bookID, err := strconv.Atoi(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	var book model.Book
+
+	// リクエストボディからデータを取得
+	if err := ctx.ShouldBindJSON(&book); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// リクエストボディーに含まれるIDを更新対象のIDとして設定
+	book.Id = bookID
+
+	fmt.Println("Bookリクエストデータ:", book)
+
+	updatedBook, err := UpdateBook(db.DB, book)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"book": updatedBook})
+}
+
+func handleDeleteBook() {}
+
+func handleDeleteAllBook() {}
